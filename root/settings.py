09a44dacee025f488b0e5acb30251c6fc45c1ef9
Developer <zhumabaevasadbek123@gmail.com>
2024-11-11 16:22:08 +0500

    master

--- root/settings.py
+++ root/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SERVER_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True')
+DEBUG = os.getenv('DEBUG', 'True') == True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
