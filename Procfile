9c1fc2a2fba40cdadfa2bcb87225b553dac3098c
Developer <zhumabaevasadbek123@gmail.com>
2024-11-15 02:17:25 +0500

    master

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn root.wsgi --log-file -
-web: ./entrypoint.sh gunicorn myproject.wsgi:application --bind 0.0.0.0:$PORT
--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-# Now start the application
-exec "$@"
